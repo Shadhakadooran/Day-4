f2785b4f837505cee78f7166d798b47f55092baf
Shadhaa <shadhakadooran75@gmail.com>
2026-06-08 15:43:53 +0530

    Add files via upload

---
+++ grade_calculator.py
@@ -0,0 +1,12 @@
+student_name = input("Enter Student Name: ")
+marks = int(input("Enter Marks: "))
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C")
+else:
+    print("Grade: D")
+    
